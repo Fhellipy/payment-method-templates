c16efb8ba276ed0b59f603697df59ed12fe24e33
Fhellipy Santana <ffhellipyc.santana@gmail.com.br>
2023-10-26 18:05:55 -0300

    feat: add business page

--- src/app/root/[businessID]/page.tsx
+++ src/app/root/[businessID]/page.tsx
@@ -1,9 +1,21 @@
+import Link from 'next/link';
+
 interface Params {
-  params: { businessID: string }
+  params: { businessID: string };
 }
 
-export default function Page({ params }: Params): JSX.Element {
-  console.log(params.businessID)
+export default function BusinessPage({ params }: Params): JSX.Element {
+  return (
+    <div className="flex flex-col items-center gap-4 p-2">
+      <Link href="/">
+        <button className="text-primary-background rounded border bg-secondary p-2">
+          Voltar
+        </button>
+      </Link>
 
-  return <h1>Teste</h1>
+      <div className="flex h-60 w-60 items-center justify-center rounded bg-primary">
+        <h1 className="text-primary-foreground">Tema #{params.businessID}</h1>
+      </div>
+    </div>
+  );
 }
