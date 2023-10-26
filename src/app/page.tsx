7aea344b42869de5c185df01c831f446b0f650b5
Fhellipy Santana <ffhellipyc.santana@gmail.com.br>
2023-10-26 18:03:16 -0300

    feat: add homepage initial content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Link from 'next/link';
+import { Header } from '../components';
+
 export default function Home(): JSX.Element {
   return (
-    <main className=" flex flex-col items-center justify-between">
+    <div className="flex w-full flex-col items-center gap-4">
+      <Header />
+
       <h1>Home</h1>
-    </main>
-  )
+
+      <nav className="flex gap-4 rounded border bg-background p-2 text-foreground">
+        <Link href="/root/1">Tema 1</Link>
+        <Link href="/root/2">Tema 2</Link>
+        <Link href="/root/3">Tema 3</Link>
+      </nav>
+    </div>
+  );
 }
