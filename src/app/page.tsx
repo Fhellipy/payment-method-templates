0a0e2e761f8a42c3aa4d79478b56f652bac61846
fhellipy.santana <ffhellipyc.santana@gmail.com>
2023-10-29 21:18:22 -0300

    feat: add mock raffles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,49 @@
-import Link from 'next/link';
-import { Header } from '../components';
+import { Cards } from '@/components/ui';
+
+const raffles = [
+	{
+		id: 1,
+		name: 'Show de prêmios 2023',
+		date: '10/11/2023',
+		price: 0.25,
+		img: 'https://rastreketec.com.br/wp-content/uploads/2021/09/f0e083d3d680bb95dbf8d68f4bcea8ed042960bcf1a1e78e7bf729772ea1c9c3.png',
+	},
+	{
+		id: 2,
+		name: 'Show de prêmios 2023',
+		date: '10/11/2023',
+		price: 0.25,
+		img: 'https://rastreketec.com.br/wp-content/uploads/2021/09/f0e083d3d680bb95dbf8d68f4bcea8ed042960bcf1a1e78e7bf729772ea1c9c3.png',
+	},
+	{
+		id: 3,
+		name: 'Show de prêmios 2023',
+		date: '10/11/2023',
+		price: 0.25,
+		img: 'https://rastreketec.com.br/wp-content/uploads/2021/09/f0e083d3d680bb95dbf8d68f4bcea8ed042960bcf1a1e78e7bf729772ea1c9c3.png',
+	},
+	{
+		id: 4,
+		name: 'Show de prêmios 2023',
+		date: '10/11/2023',
+		price: 0.25,
+		img: 'https://rastreketec.com.br/wp-content/uploads/2021/09/f0e083d3d680bb95dbf8d68f4bcea8ed042960bcf1a1e78e7bf729772ea1c9c3.png',
+	},
+	{
+		id: 5,
+		name: 'Show de prêmios 2023',
+		date: '10/11/2023',
+		price: 0.25,
+		img: 'https://rastreketec.com.br/wp-content/uploads/2021/09/f0e083d3d680bb95dbf8d68f4bcea8ed042960bcf1a1e78e7bf729772ea1c9c3.png',
+	},
+	{
+		id: 6,
+		name: 'Show de prêmios 2023',
+		date: '10/11/2023',
+		price: 0.25,
+		img: 'https://rastreketec.com.br/wp-content/uploads/2021/09/f0e083d3d680bb95dbf8d68f4bcea8ed042960bcf1a1e78e7bf729772ea1c9c3.png',
+	},
+];
 
 export default function Home(): JSX.Element {
 	return (
