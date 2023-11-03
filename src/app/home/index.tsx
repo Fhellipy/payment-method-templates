26fa646304c6d976a49b5e77761c88d6a5e79f9a
fhellipy.santana <ffhellipyc.santana@gmail.com>
2023-11-02 22:14:01 -0300

    fea: add home module

---
+++ src/app/home/index.tsx
@@ -0,0 +1,15 @@
+import { raffles } from '@/mock/raffles';
+import { Cards } from './components';
+
+export default function Home(): JSX.Element {
+	return (
+		<main className="mx-auto max-w-[85rem]">
+			<Cards items={raffles} />
+
+			<div id="scrollspy" className="p-2 sm:px-6 lg:px-8">
+				<div id="winners">Ganhadores</div>
+				<div id="sweepstakes">Sorteios</div>
+			</div>
+		</main>
+	);
+}
