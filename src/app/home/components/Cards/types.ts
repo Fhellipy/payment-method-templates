8a8a7abf401dc6419b2919830672063f628a1713
fhellipy.santana <ffhellipyc.santana@gmail.com>
2023-11-02 22:16:49 -0300

    feat: add card typing

---
+++ src/app/home/components/Cards/types.ts
@@ -0,0 +1,7 @@
+export type CardType = {
+	id: number;
+	img: string;
+	name: string;
+	date: string;
+	price: number;
+};
---
+++ src/app/root/[businessID]/components/Cards/types.ts
@@ -0,0 +1,8 @@
+export type CardType = {
+	id: number;
+	img: string;
+	name: string;
+	date: string;
+	price: number;
+	description: string;
+};
