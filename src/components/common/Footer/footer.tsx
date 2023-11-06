35ace929feeb27f701688878bf7138fca9b4f403
fhellipy.santana <ffhellipyc.santana@gmail.com>
2023-11-05 22:53:20 -0300

    feat: add footer component

---
+++ src/components/common/Footer/footer.tsx
@@ -0,0 +1,29 @@
+import { InstagramIcon } from 'lucide-react';
+import { BsWhatsapp } from 'react-icons/bs';
+
+type FooterProps = {
+	social: {
+		instagram: string;
+		whatsapp: string;
+	};
+};
+
+export function Footer({ social }: FooterProps): JSX.Element {
+	return (
+		<footer className="flex min-h-[5rem] w-full flex-col-reverse flex-wrap items-center justify-center gap-2 bg-background bg-black p-2 px-6 text-sm text-white sm:flex-row sm:flex-nowrap sm:justify-between">
+			<span className="text-center">
+				© 2023 Nome da Empresa, Inc. Todos os direitos reservados.
+			</span>
+
+			<div className="mt-1 flex items-center gap-2">
+				<a href={social?.whatsapp} className="cursor-pointer text-[1.2rem]">
+					<BsWhatsapp />
+				</a>
+
+				<a href={social?.instagram}>
+					<InstagramIcon size={20} className="cursor-pointer" />
+				</a>
+			</div>
+		</footer>
+	);
+}
