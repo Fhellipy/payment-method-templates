5e635ea1c0c2d036018e0436f48b8fe989ef9f62
fhellipy.santana <ffhellipyc.santana@gmail.com>
2023-11-05 22:51:49 -0300

    feat: export regex

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,2 @@
-export * from './utils/cn';
-export * from './utils/create-css-string';
-export * from './utils/theme-by-business-id';
-export * from './utils/to-brl';
+export * from './cn';
+export * from './regex';
