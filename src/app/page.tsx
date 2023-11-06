e241b1688721aa87b561b13a2a1ed9b11df9a70c
fhellipy.santana <ffhellipyc.santana@gmail.com>
2023-11-05 22:39:10 -0300

    feat: add header and footer components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import { Footer, Header } from '@/components';
 import Home from './home';
 
 export default function HomePage(): JSX.Element {
-	return <Home />;
+	return (
+		<>
+			<Header logo="Logo" />
+			<Home />
+			<Footer social={{ instagram: '', whatsapp: '' }} />
+		</>
+	);
 }
