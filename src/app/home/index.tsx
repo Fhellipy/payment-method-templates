dc8047c70e55020733ea7aa7365b8c2176f336f3
fhellipy.santana <ffhellipyc.santana@gmail.com>
2023-11-05 22:39:38 -0300

    feat: add news padding

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -3,7 +3,7 @@ import { Cards } from './components';
 
 export default function Home(): JSX.Element {
 	return (
-		<main className="mx-auto max-w-[85rem]">
+		<main className="mx-auto max-w-[85rem] sm:px-6 lg:px-8">
 			<Cards items={raffles} />
 
 			<div id="scrollspy" className="p-2 sm:px-6 lg:px-8">
