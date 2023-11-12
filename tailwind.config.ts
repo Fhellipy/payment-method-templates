4a1b6e86bdbfdba9d02f5e170443b1478ed94039
fhellipy.santana <ffhellipyc.santana@gmail.com>
2023-11-12 19:57:41 -0300

    feat: adds rotating animation keyframe

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -29,6 +29,15 @@ module.exports = {
 				md: 'calc(var(--radius) - 2px)',
 				sm: 'calc(var(--radius) - 4px)',
 			},
+			keyframes: {
+				rotate: {
+					'0%': { transform: 'rotate(0deg)' },
+					'100%': { transform: 'rotate(360deg)' },
+				},
+			},
+			animation: {
+				rotate: 'rotate 1s linear infinite',
+			},
 		},
 	},
 	plugins: [require('preline/plugin')],
