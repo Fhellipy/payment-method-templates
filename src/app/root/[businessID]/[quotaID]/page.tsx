b43c0f164765b20e3146beabb5b7c246a1711ab7
fhellipy.santana <ffhellipyc.santana@gmail.com>
2024-02-25 22:24:06 -0300

    refactor: add quota mock

--- src/app/root/[businessID]/[quotaID]/page.tsx
+++ src/app/root/[businessID]/[quotaID]/page.tsx
@@ -3,22 +3,11 @@ import {
 	CardImage,
 	Description,
 	SelectQuantityQuota,
-} from './components';
+} from '@modules/quota';
+import { QUOTA } from '@shared/mock';
 
-interface Params {
+type Params = {
 	params: { quotaID: string };
-}
-
-const quotaMock = {
-	title: 'Show de Prêmios',
-	date: '2023/10/11',
-	description:
-		'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet',
-	location: 'Loteria Federal 🍀',
-	img: {
-		url: 'https://rastreketec.com.br/wp-content/uploads/2021/09/f0e083d3d680bb95dbf8d68f4bcea8ed042960bcf1a1e78e7bf729772ea1c9c3.png',
-		alt: 'Show de Prêmios',
-	},
 };
 
 export default function QuotaPage({ params }: Params): JSX.Element {
@@ -28,13 +17,13 @@ export default function QuotaPage({ params }: Params): JSX.Element {
 		<main className="flex flex-col items-center gap-4 px-2 py-4 sm:px-6 lg:px-8">
 			<article className="flex w-full max-w-[85rem] flex-col gap-4">
 				<CardImage
-					title={quotaMock.title}
-					location={quotaMock.location}
-					date={quotaMock.date}
-					img={quotaMock.img}
+					title={QUOTA.title}
+					location={QUOTA.location}
+					date={QUOTA.date}
+					img={QUOTA.img}
 				/>
 
-				<Description description={quotaMock.description} />
+				<Description description={QUOTA.description} />
 
 				<SelectQuantityQuota />
 
