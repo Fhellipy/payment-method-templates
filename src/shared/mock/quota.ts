81eab596f1575ba7e5abe85b7b54a1ddcd77df7e
fhellipy.santana <ffhellipyc.santana@gmail.com>
2024-02-25 23:02:33 -0300

    feat: adds mock quota

---
+++ src/shared/mock/quota.ts
@@ -0,0 +1,11 @@
+export const QUOTA = {
+	title: 'Show de Prêmios',
+	date: '2023/10/11',
+	description:
+		'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet',
+	location: 'Loteria Federal 🍀',
+	img: {
+		url: 'https://www.designi.com.br/images/preview/10169748.jpg',
+		alt: 'Show de Prêmios',
+	},
+};
