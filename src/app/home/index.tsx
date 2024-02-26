853c44e9952aef34b8ed9fbf4db8a151f6ebc35a
fhellipy.santana <ffhellipyc.santana@gmail.com>
2024-02-25 22:20:41 -0300

    refactor: add quota card list component

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,15 +1,10 @@
-import { raffles } from '@/mock/raffles';
-import { Cards } from './components';
+import { RAFFLES } from '@/shared/mock';
+import { QuotaCardList } from '@modules/home';
 
 export default function Home(): JSX.Element {
 	return (
 		<main className="mx-auto max-w-[85rem] sm:px-6 lg:px-8">
-			<Cards items={raffles} />
-
-			<div id="scrollspy" className="p-2 sm:px-6 lg:px-8">
-				<div id="winners">Ganhadores</div>
-				<div id="sweepstakes">Sorteios</div>
-			</div>
+			<QuotaCardList items={RAFFLES} />
 		</main>
 	);
 }
