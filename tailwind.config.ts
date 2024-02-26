50fff89460065240f4b0cdcc2c34fcede5360f52
fhellipy.santana <ffhellipyc.santana@gmail.com>
2024-02-25 22:14:48 -0300

    feat: add animation style setting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,27 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss';
+
+const config = {
+	darkMode: ['class'],
 	content: [
-		'./app/**/*.{js,ts,jsx,tsx,mdx}',
-		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
-		'./components/**/*.{js,ts,jsx,tsx,mdx}',
-		'./src/**/*.{js,ts,jsx,tsx,mdx}',
-		'node_modules/preline/dist/*.js',
+		'./pages/**/*.{ts,tsx}',
+		'./components/**/*.{ts,tsx}',
+		'./app/**/*.{ts,tsx}',
+		'./src/**/*.{ts,tsx}',
 	],
+	prefix: '',
 	theme: {
+		container: {
+			center: true,
+			padding: '2rem',
+			screens: {
+				'2xl': '1400px',
+			},
+		},
 		extend: {
 			colors: {
 				border: 'hsl(var(--border))',
+				input: 'hsl(var(--input))',
 				ring: 'hsl(var(--ring))',
-
 				background: 'hsl(var(--background))',
 				foreground: 'hsl(var(--foreground))',
 				primary: {
@@ -30,15 +39,22 @@ module.exports = {
 				sm: 'calc(var(--radius) - 4px)',
 			},
 			keyframes: {
-				rotate: {
-					'0%': { transform: 'rotate(0deg)' },
-					'100%': { transform: 'rotate(360deg)' },
+				'accordion-down': {
+					from: { height: '0' },
+					to: { height: 'var(--radix-accordion-content-height)' },
+				},
+				'accordion-up': {
+					from: { height: 'var(--radix-accordion-content-height)' },
+					to: { height: '0' },
 				},
 			},
 			animation: {
-				rotate: 'rotate 1s linear infinite',
+				'accordion-down': 'accordion-down 0.2s ease-out',
+				'accordion-up': 'accordion-up 0.2s ease-out',
 			},
 		},
 	},
-	plugins: [require('preline/plugin')],
-};
+	plugins: [require('tailwindcss-animate')],
+} satisfies Config;
+
+export default config;
