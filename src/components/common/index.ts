6fe1e49abed9fe44616aa54d8b5ddf81555cc7d7
fhellipy.santana <ffhellipyc.santana@gmail.com>
2024-02-25 21:59:35 -0300

    refactor: realocar arquivo

--- src/components/common/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './Footer';
-export * from './Form';
-export * from './GlobalProvider';
-export * from './Header';
-export * from './Loading';
--- src/test/page.test.tsx
+++
@@ -1,8 +0,0 @@
-import Home from '@/app/page';
-import { render, screen } from '@testing-library/react';
-
-test('renders the home page', () => {
-	render(<Home />);
-	const headingElement = screen.getByText(/Home/i);
-	expect(headingElement).toBeInTheDocument();
-});
