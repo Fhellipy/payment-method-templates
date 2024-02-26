2f7289ceb405c78823d4ed840e31384d428716a4
fhellipy.santana <ffhellipyc.santana@gmail.com>
2024-02-25 23:02:19 -0300

    feat: adds mock banners

---
+++ src/shared/mock/banners.ts
@@ -0,0 +1,17 @@
+export const BANNERS = [
+	{
+		id: 1,
+		src: 'https://www.designi.com.br/images/preview/10169748.jpg',
+		alt: 'Image 1',
+	},
+	{
+		id: 2,
+		src: 'https://www.designi.com.br/images/preview/10169748.jpg',
+		alt: 'Image 2',
+	},
+	{
+		id: 3,
+		src: 'https://www.designi.com.br/images/preview/10169748.jpg',
+		alt: 'Image 3',
+	},
+];
