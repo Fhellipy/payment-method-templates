47373b27f786e144089d8bf9b43c40732385ce0b
Fhellipy Santana <ffhellipyc.santana@gmail.com>
2024-02-27 17:40:00 -0300

    feat: adds credit card template

---
+++ src/modules/payment/components/credit-card-template/credit-card-template.tsx
@@ -0,0 +1,87 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+	creditCardSchema,
+	maskCardExpiration,
+	maskCreditCardHolder,
+	maskCreditCardNumber,
+	maskCreditCardSecurityCode,
+	type CreditCardFormValues,
+} from '@modules/payment';
+import { Button, Form, InputForm } from '@shared/components';
+import { useForm, type SubmitHandler } from 'react-hook-form';
+import { AcceptedCards } from './accepted-cards';
+
+export function CreditCardTemplate() {
+	const form = useForm<CreditCardFormValues>({
+		resolver: zodResolver(creditCardSchema),
+	});
+
+	const onSubmit: SubmitHandler<CreditCardFormValues> = (data) => {
+		console.log(data);
+
+		alert('Pedido finalizado com sucesso!');
+	};
+
+	return (
+		<div>
+			<Form {...form}>
+				<form
+					method="POST"
+					onSubmit={form.handleSubmit(onSubmit)}
+					className="flex flex-col gap-3 p-2"
+				>
+					<InputForm
+						name="cardNumber"
+						label="Número do cartão"
+						placeholder="0000 0000 0000 0000"
+						control={form.control}
+						onChange={maskCreditCardNumber}
+						uppercase
+						min={16}
+						max={23}
+					/>
+
+					<InputForm
+						name="holderName"
+						label="Nome do titular"
+						placeholder="Digite o nome como está no cartão..."
+						control={form.control}
+						onChange={maskCreditCardHolder}
+						uppercase
+					/>
+
+					<div className="flex flex-col gap-2 sm:flex-row">
+						<InputForm
+							name="cardExpiration"
+							label="Data de expiração"
+							placeholder="MM/AA"
+							autoComplete="off"
+							control={form.control}
+							onChange={maskCardExpiration}
+							uppercase
+							max={5}
+						/>
+
+						<InputForm
+							name="cardSecurityCode"
+							label="CVV"
+							placeholder="123"
+							autoComplete="off"
+							control={form.control}
+							onChange={maskCreditCardSecurityCode}
+							isInputPassword
+							uppercase
+							min={3}
+						/>
+					</div>
+
+					<Button type="submit">Finalizar Compra</Button>
+				</form>
+			</Form>
+
+			<AcceptedCards />
+		</div>
+	);
+}
