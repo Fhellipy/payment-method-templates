a441438894b07b747f0273857020c2dda13ebb52
Fhellipy Santana <ffhellipyc.santana@gmail.com>
2024-02-27 17:53:30 -0300

    feat: add regex that only allows letters and no special characters

--- src/shared/lib/regex.ts
+++ src/shared/lib/regex.ts
@@ -12,3 +12,19 @@
  * Este regex pode ser usado para remover todos os caracteres que não numéricos de uma string
  */
 export const onlyNumberRegex = /\D/g;
+
+/**
+ * Este regex só passa quando todos os elementos forem palavras, ou seja, letras do alfabeto sem caracteres especiais
+ *
+ * Exemplos de texto condizente:
+ * - `ABCDEF`
+ *
+ * Exemplos de texto incorreto:
+ * - `012345`
+ * - `ABC123`
+ * - `123ABC`
+ * - `ÃBÇ-DEF`
+ *
+ * Este regex pode ser usado para remover todos os números e caracteres especiais de uma string
+ */
+export const onlyWordsRegex = /[^a-z\u00C0-\u00FF\s]/gi;
