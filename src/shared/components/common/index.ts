d65eb91d2ad20b3c2292bb6477792b579acd6e84
Fhellipy Santana <ffhellipyc.santana@gmail.com>
2024-02-27 17:48:46 -0300

    feat: exports form inputs components

--- src/shared/components/common/index.ts
+++ src/shared/components/common/index.ts
@@ -1,5 +1,7 @@
 export * from './form';
 export * from './generic-error-boundary';
 export * from './global-providers';
+export * from './input-form';
+export * from './input-password';
 export * from './loading';
 export * from './nav';
