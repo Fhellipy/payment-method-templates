8974cb16b5b9dca0ffe84e2e45de497f5c42b894
fhellipy.santana <ffhellipyc.santana@gmail.com>
2024-02-27 22:26:05 -0300

    feat: exports common components

--- src/shared/components/common/index.ts
+++ src/shared/components/common/index.ts
@@ -3,5 +3,3 @@ export * from './generic-error-boundary';
 export * from './global-providers';
 export * from './input-form';
 export * from './input-password';
-export * from './loading';
-export * from './nav';
